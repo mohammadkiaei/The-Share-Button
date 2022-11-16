2e9eae36c0325977633a0fad85bee548549903bf
mohammadkiaei <s.kiaei.s.m.m.k1380@gmail.com>
2022-11-16 10:13:39 +0330

    Initial commit

---
+++ main.js
@@ -0,0 +1,9 @@
+let socialLink = document.querySelector('.social-link');
+let shareLink = document.querySelector('.share-link');
+
+socialLink.addEventListener('click', function () {
+    socialLink.classList.toggle('active');
+    shareLink.classList.toggle('active');
+})
+
+// Developed By SeyedMohammadMahdi Kiaei
